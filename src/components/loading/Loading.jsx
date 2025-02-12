852c16e12c603c3678203b6f851c0ba9862f1aae
abhigyan3819 <abhigyan3819@gmail.com>
2025-02-12 21:59:13 +0530

    Added src/components/loading/Loading.jsx

---
+++ src/components/loading/Loading.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './loading.css';
+
+const Loading = () => {
+  return (
+    <div className="loading-container">
+      <div className="loading-logo">
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="circle"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Loading;
